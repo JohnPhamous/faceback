0aa91f67580f81443d05c4422cb69cff431dac56
Rae <raeleneg@uci.edu>
2017-01-14 11:59:51 -0800

    Create phrase find.js

---
+++ server/routes/phrase/find.js
@@ -0,0 +1,19 @@
+var
+  mongoose = require('mongoose'),
+  Page = require('./../../models/Phrase');
+
+module.exports = function(router) {
+  // A GET request to /api/phrase/:url will 
+  // get all the data for a phrase
+  router.get('/phrase/:phrase', function (req, res) {
+    Phrase.findOne({ phrase: req.params.phrase }, function (err, phraseData) {
+      if (err) {
+        return res.send(err.message);
+      } else if (!phraseData) {
+        return res.send('No Phrase was found at url: ' + req.params.url);
+      }
+
+      return res.send(phraseData);
+    });
+  });
+};
