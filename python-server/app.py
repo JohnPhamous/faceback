9dea241d734c01dc3267185eef54890b0850fde1
John Pham <johnphammail@gmail.com>
2017-01-14 19:42:10 -0800

    added statuss and comment calls

--- python-server/app.py
+++ python-server/app.py
@@ -16,19 +16,6 @@ access_token = app_id + "|" + app_secret
 def request_all(url):
     req = urllib2.Request(url)
     response = urllib2.urlopen(req)
-    # success = False
-    # while success is False:
-    #     try:
-    #         response = urllib2.urlopen(req)
-    #         if response.getcode() == 200:
-    #             success = True
-    #     except Exception, e:
-    #         print e
-    #         time.sleep(5)
-    #
-    #         print "Error for URL %s: %s" % (url, datetime.datetime.now())
-    #         print "Retrying."
-
     return response.read()
 
 # Needed to write tricky unicode correctly to csv
@@ -90,7 +77,6 @@ def processFacebookPageFeedStatus(status, access_token):
             datetime.timedelta(hours=-5) # EST
     status_published = status_published.strftime(
             '%Y-%m-%d %H:%M:%S') # best time format for spreadsheet programs
-
     num_reactions = 0 if 'reactions' not in status else \
             status['reactions']['summary']['total_count']
     num_comments = 0 if 'comments' not in status else \
@@ -146,7 +132,6 @@ def scrapeFacebookPageFeedStatus(page_id, access_token):
 
         while has_next_page:
             for status in statuses['data']:
-
                 # Ensure it is a status with the expected metadata
                 if 'reactions' in status:
                     w.writerow(processFacebookPageFeedStatus(status,
@@ -163,14 +148,144 @@ def scrapeFacebookPageFeedStatus(page_id, access_token):
             else:
                 has_next_page = False
 
-
         print "\nDone!\n%s Statuses Processed in %s" % \
                 (num_processed, datetime.datetime.now() - scrape_starttime)
 
-def csv_to_json(page_id):
-    print("Converting csv to json")
+def getFacebookCommentFeedData(status_id, access_token, num_comments):
+    # Construct the URL string
+    base = "https://graph.facebook.com/v2.6"
+    node = "/%s/comments" % status_id
+    fields = "?fields=id,message,like_count,created_time,comments,attachment"
+    parameters = "&order=chronological&limit=%s&access_token=%s" % \
+        (num_comments, access_token)
+    url = base + node + fields + parameters
 
-    csv_file = open('data/%s_facebook_statuses.csv' % page_id, 'rU')
+    # retrieve data
+    data = request_all(url)
+    if data is None:
+        return None
+    else:
+        return json.loads(data)
+
+
+def processFacebookComment(comment, status_id, parent_id=''):
+
+    # The status is now a Python dictionary, so for top-level items,
+    # we can simply call the key.
+
+    # Additionally, some items may not always exist,
+    # so must check for existence first
+
+    comment_id = comment['id']
+    comment_message = '' if 'message' not in comment else \
+        unicode_normalize(comment['message'])
+    comment_likes = 0 if 'like_count' not in comment else \
+        comment['like_count']
+
+    if 'attachment' in comment:
+        attach_tag = "[[%s]]" % comment['attachment']['type'].upper()
+        comment_message = attach_tag if comment_message is '' else \
+            (comment_message.decode("utf-8") + " " +
+             attach_tag).encode("utf-8")
+
+    # Time needs special care since a) it's in UTC and
+    # b) it's not easy to use in statistical programs.
+
+    comment_published = datetime.datetime.strptime(
+        comment['created_time'], '%Y-%m-%dT%H:%M:%S+0000')
+    comment_published = comment_published + datetime.timedelta(hours=-5)  # EST
+    comment_published = comment_published.strftime(
+        '%Y-%m-%d %H:%M:%S')  # best time format for spreadsheet programs
+
+    # Return a tuple of all processed data
+
+    return (comment_id, status_id, parent_id, comment_message,
+            comment_published, comment_likes)
+
+
+def scrapeFacebookPageFeedComments(page_id, access_token):
+    with open('data/%s_facebook_comments.csv' % page_id, 'wb') as file:
+        w = csv.writer(file)
+        w.writerow(["comment_id", "status_id", "parent_id",
+                    "comment_message", "comment_published", "comment_likes"])
+
+        num_processed = 0   # keep a count on how many we've processed
+        scrape_starttime = datetime.datetime.now()
+
+        print "Scraping %s Comments From Posts: %s\n" % \
+            (page_id, scrape_starttime)
+
+        with open('data/%s_facebook_statuses.csv' % page_id, 'rb') as csvfile:
+            reader = csv.DictReader(csvfile)
+
+            #reader = [dict(status_id='759985267390294_1158001970921953')]
+
+            for status in reader:
+                has_next_page = True
+
+                comments = getFacebookCommentFeedData(status['status_id'],
+                                                      access_token, 100)
+
+                while has_next_page and comments is not None:
+                    for comment in comments['data']:
+                        w.writerow(processFacebookComment(comment,
+                                                          status['status_id']))
+
+                        if 'comments' in comment:
+                            has_next_subpage = True
+
+                            subcomments = getFacebookCommentFeedData(
+                                comment['id'], access_token, 100)
+
+                            while has_next_subpage:
+                                for subcomment in subcomments['data']:
+                                    # print (processFacebookComment(
+                                        # subcomment, status['status_id'],
+                                        # comment['id']))
+                                    w.writerow(processFacebookComment(
+                                        subcomment,
+                                        status['status_id'],
+                                        comment['id']))
+
+                                    num_processed += 1
+                                    if num_processed % 1000 == 0:
+                                        print "%s Comments Processed: %s" % \
+                                            (num_processed,
+                                             datetime.datetime.now())
+
+                                if 'paging' in subcomments:
+                                    if 'next' in subcomments['paging']:
+                                        subcomments = json.loads(
+                                            request_all(
+                                                subcomments['paging']
+                                                ['next']))
+                                    else:
+                                        has_next_subpage = False
+                                else:
+                                    has_next_subpage = False
+
+                        # output progress occasionally to make sure code is not
+                        # stalling
+                        num_processed += 1
+                        if num_processed % 1000 == 0:
+                            print "%s Comments Processed: %s" % \
+                                (num_processed, datetime.datetime.now())
+
+                    if 'paging' in comments:
+                        if 'next' in comments['paging']:
+                            comments = json.loads(request_all(
+                                comments['paging']['next']))
+                        else:
+                            has_next_page = False
+                    else:
+                        has_next_page = False
+
+        print "\nDone!\n%s Comments Processed in %s" % \
+            (num_processed, datetime.datetime.now() - scrape_starttime)
+
+def csv_to_json(page_id, kind):
+    print("Converting csv to json")
+    csv_file = open('data/{}_facebook_{}.csv'.format(page_id, kind), 'rU')
     reader = csv.DictReader(csv_file, fieldnames = ( "status_id",
                 "status_message", "link_name", "status_type",
                 "status_link", "status_published", "num_reactions",
@@ -179,29 +294,40 @@ def csv_to_json(page_id):
     out = jsonify([row for row in reader])
     print(type(out))
     return out
-    # with open('data/%s_facebook_statuses.csv' % page_id, mode='r') as infile:
-    #     reader = csv.reader(infile)
-    #     with open('data/%s_facebook_statuses.txt' % page_id, mode="w") as outfile:
-    #         writer = csv.writer(outfile)
-    #         dat_dict = {rows[0]:rows[1:] for rows in reader}
-    # return jsonify(**dat_dict)
 
 @app.route('/')
 def index():
     return "Welcome to our REST API. This is for our <b>HackUCI 2017</b> project. You can make requests with making a GET request to a Facebook page with GET /req?https://www.facebook.com/CitrusHack/ for example. <br><br> This project is by: Aaroh Mankad(UCR), Kevin Wong(UCI), John Pham(UCR), and Raelene Gonzales(UCI)."
 
+@app.route('/test', methods=['GET'])
+def send():
+    url = request.args.get('url')
+    kind = request.args.get('kind')
+    return url + " " + kind
+
 @app.route('/req', methods=['GET'])
 def get_task():
     # Input is the Facebook URL
     all_args = request.args.lists()
-    fb_url = str(all_args[0][0])
-    fb_id = fb_url.split('/')
-    if not os.path.isfile('data/%s_facebook_statuses.csv' % fb_id[3]):
-        scrapeFacebookPageFeedStatus(fb_id[3], access_token)
-    print("Data exist already, skipping scrape")
-    return csv_to_json(fb_id[3])
+    url = request.args.get('url')
+    kind = request.args.get('kind')
+    fb_id = url.split('/')
 
-    # return fb_url
+    if kind == "s" or kind == "sc":
+        if not os.path.isfile('data/%s_facebook_statuses.csv' % fb_id[3]):
+            scrapeFacebookPageFeedStatus(fb_id[3], access_token)
+            print("New query type, scraping now for statuses...")
+        else:
+            print("Repeated query, using cache for statuses")
+
+    if kind == "c" or kind == "sc":
+        if not os.path.isfile('data/%s_facebook_comments.csv' % fb_id[3]):
+            scrapeFacebookPageFeedComments(fb_id[3], access_token)
+            print("New query type, scraping now for comments...")
+        else:
+            print("Repeated query, using cache for comments")
+
+    return csv_to_json(fb_id[3], "statuses")
 
 if __name__ == '__main__':
     app.debug = True
