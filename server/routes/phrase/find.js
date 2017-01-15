e0e7e9dcc3ebff9a027bae756180ac69ee8db6f5
Rae <raeleneg@uci.edu>
2017-01-14 18:40:13 -0800

    Update find.js

--- server/routes/phrase/find.js
+++ server/routes/phrase/find.js
@@ -5,8 +5,8 @@ var
 module.exports = function(router) {
   // A GET request to /api/phrase/:url will 
   // get all the data for a phrase
-  router.get('/phrase/:phrase', function (req, res) {
-    Phrase.findOne({ phrase: req.params.phrase }, function (err, phraseData) {
+  router.get('/phrase/:pageUrl/:phrase', function (req, res) {
+    Phrase.findOne({ pageUrl: req.params.pageUrl, phrase: req.params.phrase }, function (err, phraseData) {
       if (err) {
         return res.send(err.message);
       } else if (!phraseData) {
